b1f8e67415931f6d1bda8643c733db342a468219
satoshi-nakamura-018 <toda2020-018@iniad.org>
2021-06-09 10:16:51 +0000

    fortune.htmlを追加

--- app.py
+++ app.py
@@ -4,8 +4,19 @@ app = app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    uranai = random.choice(["大吉","中吉","小吉"])
-    return render_template('index.html',fortune=uranai)
-		
+    unset_list = ["大吉","中吉","小吉"]
+    uranai = random.choice(unset_list)
+    return render_template('index.html',fortune=uranai, fortune_list=unset_list)
+
+@app.route('/fortune')
+def fortune():
+    unsei_list = ["大吉","中吉","小吉"]
+    uranai_list = [
+        {"day":"今日","fortune":random.choice(unsei_list)},
+        {"day":"明日","fortune":random.choice(unsei_list)},
+        {"day":"明後日","fortune":random.choice(unsei_list)},
+    ]
+    return render_template('fortune.html',fortune_list=uranai_list)
+
 if __name__ == "__main__":
     app.run(debug=True)
