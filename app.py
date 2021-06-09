1787e67c26c8527bdc026e070671081f18e9e773
satoshi-nakamura-018 <toda2020-018@iniad.org>
2021-06-09 09:58:33 +0000

    占いを追加

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask,render_template
+import random
 app = app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    uranai = random.choice(["大吉","中吉","小吉"])
+    return render_template('index.html',fortune=uranai)
 		
 if __name__ == "__main__":
     app.run(debug=True)
